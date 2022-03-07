0b13c13afb0991d0e7778428bd0ae0d5647d6492
facumay21 <facumay21@gmail.com>
2022-03-06 21:24:23 -0300

    suba del proyecto final

---
+++ src/components/data.js
@@ -0,0 +1,23 @@
+const data = {
+        products: [
+        {
+            id: '1',
+            name: 'Pack fotos de 15',
+            price: 1400,
+            image: 'https://picsum.photos/id/180/2400/1600',
+        },
+        {
+            id: '2',
+            name: 'Pack Videoclip',
+            price: 2400,
+            image: 'https://picsum.photos/id/111/4400/2656',
+        },
+        {
+            id: '3',
+            name: 'Pack "tu primer año" ',
+            price: 1000,
+            image: 'https://picsum.photos/id/21/3008/2008',
+        },
+        ],
+    };
+    export default data;
